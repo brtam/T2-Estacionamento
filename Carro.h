6cb476f8bdc92a7b1412fe7a2267f336e16118ec
GIOVANI TOMEDI <96105641@l1726926.portoalegre.pucrsnet.br>
2015-11-03 20:25:07 -0200

    Classe Carro

---
+++ Carro.h
@@ -0,0 +1,22 @@
+#ifndef CARRO_H
+#define CARRO_H
+
+#include <iostream>
+
+class Carro
+{
+    private:
+        string placa;
+        string modelo;
+        string cor;
+        int h_in, m_in, s_in, h_out, m_out, s_out,
+        int h_total;
+        int saldo;
+
+    public:
+        Carro(string placa, string modelo, string cor, int h_in, int m_in, int s_in, int h_out, int m_out, int s_out);
+        void getInfo(string placa);
+
+};
+
+#endif // CARRO_H
