a84d1aebb262566cde3b0bad7b456a49e073d101
BRUNO GALDINO TAMBASCO <12202155@l1727023.portoalegre.pucrsnet.br>
2015-11-03 20:29:28 -0200

    classepagamento

---
+++ Classe_Pagamento.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+class Pagamento {
+
+private: void pagamento;
+
+public: float saldo();
+	void mostrar();
+	void saldo_suficiente();
+
+};
+
+Pagamento::Pagamento()
+{
+	saldo=s;
+}
+
+
